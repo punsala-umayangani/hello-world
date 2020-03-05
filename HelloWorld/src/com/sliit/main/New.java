811408c8f08de46d4ab381c7cd7a35290ea7f2a1
punsala-umayangani <umayanganipunsala@gmail.com>
2020-03-04 20:05:06 -0800

    2nd commit

---
+++ HelloWorld/src/com/sliit/main/New.java
@@ -0,0 +1,10 @@
+package com.sliit.main;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("this is a new class");
+	}
+
+}
